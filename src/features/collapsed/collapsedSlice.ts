251b5f6dd26a1f6ddc93a0353dd2854ff94fe02e
张一 <2326962137@qq.com>
2020-11-03 08:51:13 +0800

    2020-11-03 08:50:42

---
+++ src/features/collapsed/collapsedSlice.ts
@@ -0,0 +1,5 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '@/app/rootReducer';
+// import { AppThunk } from '@/app/store';
+
+const initialState: boolean = false;
